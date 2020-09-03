8409055c8c92d1e6a9cf1d710085b08f8ceba2c7
tandf <tandf.luo@gmail.com>
2020-09-03 23:31:20 +0800

    install oh-my-zsh automatically

--- README.md
+++ README.md
@@ -1,24 +1,6 @@
 # Dotfiles
 Install:
 
-First you need to have a local vim resource file. For Linux users, 
-this file is automatically copied from `vim\.vimrc_local_sample` 
-by running `./install.sh ` if there doesn't exist such a file.
-
-You can also write one yourself. You can take `vim\.vimrc_local_sample`
- or `vim\_vimrc_local_sample` as an example.  The local file name 
-should be `.vimrc_local` or `_vimrc_local`, depending on your OS. You 
-can also simply use the sample file by copy it and change its name.
-
 Run install script:
 
 ```./install.sh```
-
-## TODO
-- [ ] Split vimrc according to function. Like [this repo](https://github.com/skywind3000/vim-init).
-  - [x] Specific viriable for specific OS.
-- [x] Add `source ~/.bashrc_tandf` to .bashrc automatically.
-- [x] Custom status line.
-    - [x] Show word count for md and similar files.
-- [ ] A better colorscheme (than solarized).
-- [ ] pandoc md to html
--- install.sh
+++ install.sh
@@ -1,13 +1,28 @@
+stow -t $HOME vim tmux bash nvim zsh
+
+# UltiSnips requires specific path
+if [ ! -d ~/.vim/plugged/ultisnips/UltiSnips ]; then
+    mkdir -p ~/.vim/plugged/ultisnips/UltiSnips
+fi
+stow -t ~/.vim/plugged/ultisnips/UltiSnips UltiSnips
+
+# Install oh-my-zsh
+if [ ! -d ~/.oh-my-zsh ]; then
+    echo "========== installing oh-my-zsh =========="
+    # not working due to "network problems" that everyone knows
+    # sh -c "$(proxychains curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+    sh misc/ohmyzsh-install.sh
+fi
+
+# install incr.zsh
+if [ ! -e ~/.oh-my-zsh/plugins/incr/incr*.zsh ]; then
+    echo "========== installing incr =========="
+    mkdir ~/.oh-my-zsh/plugins/incr -p
+    cp misc/incr-0.2.zsh ~/.oh-my-zsh/plugins/incr/
+fi
+
 # Automatically add source command to .zshrc
 grep -qxF "[ -f ~/.zshrc_tandf ] && source ~/.zshrc_tandf" ~/.zshrc ||
     ( echo "[ -f ~/.zshrc_tandf ] && source ~/.zshrc_tandf" >> ~/.zshrc &&
 echo "Write to .zshrc" )
 
-
-stow -t $HOME vim tmux bash nvim zsh
-
-# UltiSnips requires specific path
-if [ ! -d "~/.vim/plugged/ultisnips/UltiSnips" ]; then
-    mkdir -p ~/.vim/plugged/ultisnips/UltiSnips
-fi
-stow -t ~/.vim/plugged/ultisnips/UltiSnips UltiSnips
