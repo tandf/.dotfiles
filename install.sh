a9d045a72ed08af28b08854a5d143cecc638710f
tandf <tandf.luo@gmail.com>
2020-12-20 15:05:39 +0800

    zsh plugin

--- install.sh
+++ install.sh
@@ -5,21 +5,32 @@ if [ ! -d ~/.oh-my-zsh ]; then
 fi
 
 # install incr.zsh
-if [ ! -e ~/.oh-my-zsh/plugins/incr/incr*.zsh ]; then
+if [ ! -e ~/.oh-my-zsh/custom/plugins/incr/incr*.zsh ]; then
     echo "========== installing incr =========="
-    mkdir ~/.oh-my-zsh/plugins/incr -p
-    cp misc/incr-0.2.zsh ~/.oh-my-zsh/plugins/incr/
+    mkdir ~/.oh-my-zsh/custom/plugins/incr -p
+    cp misc/incr-0.2.zsh ~/.oh-my-zsh/custom/plugins/incr/
 fi
 
-stow -t $HOME vim tmux bash nvim zsh
-
-# UltiSnips requires specific path
-if [ -d ~/.vim/plugged/ultisnips/UltiSnips ]; then
-    stow -t ~/.vim/plugged/ultisnips/UltiSnips UltiSnips
+# install zsh-autosuggestion
+if [ ! -d ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ]; then
+    echo "========== installing zsh-autosuggestions =========="
+    git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 fi
 
 # Automatically add source command to .zshrc
 grep -qxF "[ -f ~/.zshrc_tandf ] && source ~/.zshrc_tandf" ~/.zshrc ||
     ( echo "[ -f ~/.zshrc_tandf ] && source ~/.zshrc_tandf" >> ~/.zshrc &&
-echo "Write to .zshrc" )
+echo "========== Write \"source ~/.zshrc_tandf\" to .zshrc ==========" )
+
+# Setup zsh plugins
+plugins="git zsh-autosuggestions"
+sed -i "s/^plugins=.*$/plugins=($plugins)/g" ~/.zshrc
+
+stow -t $HOME vim tmux bash zsh
+stow
+
+# UltiSnips requires specific path
+if [ -d ~/.vim/plugged/ultisnips/UltiSnips ]; then
+    stow -t ~/.vim/plugged/ultisnips/UltiSnips UltiSnips
+fi
 
